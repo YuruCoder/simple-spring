7b1a9c235118fec5019d19f7d1d0dbdf23ba88dc
Hojun Kim <76956067+hojunkim1@users.noreply.github.com>
2023-01-05 22:48:07 +0900

    mvc basic

--- src/main/java/com/example/simplespring/controller/MemberController.java
+++ src/main/java/com/example/simplespring/controller/MemberController.java
@@ -1,8 +1,12 @@
 package com.example.simplespring.controller;
 
+import com.example.simplespring.domain.Member;
 import com.example.simplespring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -12,5 +16,25 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    private String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("members/new")
+    public String create(MemberForm form) {
+        var member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping("members")
+    public String list(Model model) {
+        var members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
